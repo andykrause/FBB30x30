e4768e11454b386548bd4a5ba915b72ecf367927
Andy Krause <andykrause19@gmail.com>
2019-01-27 07:30:18 -0800

    Add: Initial relative value calculations #4
    Simple relative value projects ignoring team and position

--- R/analyzeSeasons.R
+++ R/analyzeSeasons.R
@@ -22,7 +22,9 @@ seasonAnalysis <- function(season,
   
   x_mat <- season %>% 
     dplyr::select(-tidyselect::contains('starts'),
-                  -tidyselect::contains('moves'), -rank, -team)
+                  -tidyselect::contains('moves'), 
+                  -tidyselect::contains('rank'),
+                  -tidyselect::contains('team'))
   
   data.frame(category = names(x_mat),
              diff = unlist(lapply(x_mat, categoryDiffEngine, ...)),
@@ -40,9 +42,13 @@ seasonAnalysis <- function(season,
 #' @importFrom stats median
 #' @export
 
-categoryDiffEngine <- function(x, metric = 'median'){
+categoryDiffEngine <- function(x, metric = 'median', trim = FALSE){
   
   calcMetric <- get(metric)
-  xx <- sort(x)
+  if (trim){
+    xx <- sort(x)[2:(length(x) -1)]
+  } else {
+    xx <- sort(x)
+  }
   round(abs(calcMetric(xx[-1] - xx[-length(xx)])),3)
 }
---
+++ scripts/sandbox.R
@@ -0,0 +1,59 @@
+
+library(FBB30x30)
+library(tidyverse)
+
+
+## Make OBP and SLG adjustments
+## Document functions
+## Pass rv, rp etc. up through results
+## Make constraints for all RV values
+
+
+setConfigs <- function(nbr_owners = 8,
+                       season_year = 2019,
+                       ...){
+  
+  scoring_df <- data.frame(stat = c('r', 'hr', 'rbi', 'so', 'sb', 'slg', 'obp', 'e', 
+                                    'gidp', 'ip', 'k', 'qs', 'whip', 'holds', 'sv'),
+                           class = c(rep('hit', 9), rep('pitch', 6)),
+                           category = c('+','+','+','-','+','+','+','-','-','+','+','+',
+                                        '-','+','+'),
+                           method = c(rep('count', 5), 'ratio', 'ratio', rep('count', 5),
+                                      'ratio', 'count', 'count'))
+  
+  roster_df <- tibble(roster = c('C', '1B', '2B', 'SS', '3B', 'LF', 'CF', 'RF', 'OF', 
+                                 'DH', 'MI', 'CI', 'IF', 'Util', 'SP', 'RP', 'P'),
+                      type = c(rep('hit', 14), rep('pitch', 3)),
+                      position = list('C', '1B', '2B', 'SS', '3B', 'LF', 'CF', 'RF',
+                                      c('CF', 'RF', 'LF'), 'DH', c('2B', 'SS'), 
+                                      c('1B', '3B'), c('1B', '2B', '3B', 'SS'),
+                                      c('C', '1B', '2B', '3B', 'SS', 'LF','CF', 'RF', 'DH'),
+                                      'SP', 'RP', c('SP', 'RP')),
+                      count = c(2, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 2, 5, 4, 3),
+                      funnel = c(1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 2, 2, 4, 9, 1, 1, 2),
+                      priority = c(1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 2, 2, 3, 4, 1, 1, 2)) %>%
+    dplyr::mutate(population = count * nbr_owners)
+  
+  structure(list(season = season_year,
+                 nbr_owners = nbr_owners,
+                 roster = roster_df,
+                 scoring = scoring_df),
+            class = 'fbbConfigs')
+}
+
+configs <- setConfigs(season_year = 2019)
+
+
+projs_ <- list(bat = batprojs_df,
+              pitch = pitchprojs_df)
+stats_ <- list(bat = batting_df,
+               pitch = pitching_df,
+               field = fielding_df)
+
+relval_df <- setRelValue(projs_, stats_, configs)
+relval_df
+
+
+
+
+
